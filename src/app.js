7ce6d4f2fc03f582ff266d2e5aa208d89d0001c7
your-username <anmol9456484142@gmail.com>
2026-02-20 21:52:46 +0530

    done

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const app = {
+  init(options = {}) {
+    this.config = options;
+    return this;
+  }
+};
+export default app
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-// utils vs controller
 import mongoose from "mongoose"
 import {DB_NAME} from "./constants.js"
-
-import   app from "./app.js"
-;(async ()={
+import app from "./app.js"
+import connectDB from "./db/index.js"
+import dotenv from 'dotenv'
+connectDB()
+dotenv.config(
+    {
+       path: '.env'
+    }
+)
+/*
+;(async () => {
     try {
-        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error",(error)=>{
+        console.log("error",error);
+        throw error
+    }),
+    app.listen(process.env.PORT)
     } catch (error) {
         console.log("error",error);
-        
+
     }
  })()
+ */
