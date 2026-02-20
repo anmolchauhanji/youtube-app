985185d5fbe3a536d9663a05ed9d5e6ea38cdf39
anmol Chauhan <anmol9456484142@gmail.com>
2026-02-20 19:27:59 +0530

    minimal changer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 // utils vs controller
+import mongoose from "mongoose"
+import {DB_NAME} from "./constants.js"
+
+import   app from "./app.js"
+;(async ()={
+    try {
+        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    } catch (error) {
+        console.log("error",error);
+        
+    }
+ })()
